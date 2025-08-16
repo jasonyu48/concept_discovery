641074c29ada0eaae889a57318819cf5c0c96b42
jasonyu48 <jasonyu12139@gmail.com>
2025-08-16 03:06:46 -0400

    another 15 actions

--- tdmpc2/tdmpc2.py
+++ tdmpc2/tdmpc2.py
@@ -173,7 +173,8 @@ class TDMPC2(torch.nn.Module):
 			# if 'counting' in self.cfg.task, then action is one of the three numbers: -0.9, 0, or 0.9 with equal probability
 			if 'counting' in self.cfg.task:
 				# Sample uniformly and form an action vector
-				vals = torch.tensor([-0.99, -0.9, -0.8, -0.7, -0.6, -0.2, -0.1, 0.0, 0.1, 0.2, 0.6,0.7, 0.8, 0.9, 0.99], device=self.device)
+				# vals = torch.tensor([-0.99, -0.9, -0.8, -0.7, -0.6, -0.2, -0.1, 0.0, 0.1, 0.2, 0.6, 0.7, 0.8, 0.9, 0.99], device=self.device)
+				vals = torch.tensor([-0.99, -0.9, -0.85, -0.75, -0.7, -0.15, -0.1, 0.0, 0.1, 0.15, 0.7, 0.75, 0.85, 0.9, 0.99], device=self.device)
 				idx = torch.randint(0, 15, (), device=self.device)
 				a = torch.full((self.cfg.action_dim,), vals[idx].item(), device=self.device)
 				if self.cfg.multitask:
